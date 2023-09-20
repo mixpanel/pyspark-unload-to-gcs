85f0903c91503f17c96277551aba12df4befbf9c
Deepak Neralla <deepak.neralla@gmail.com>
2023-09-20 16:32:44 -0700

    csv format to export only one file (#3)

--- export.py
+++ export.py
@@ -1,12 +1,9 @@
 import argparse
-import pyspark
-import pyspark.sql.functions as f
-
 
 def export_to_gcs(args):
     """
-    This function takes in GCS credentials and unloads the results of a 
-    Query to GCS using spark. 
+    This function takes in GCS credentials and unloads the results of a
+    Query to GCS using spark.
     """
     spark.conf.set("google.cloud.auth.service.account.enable", "true")
     spark.conf.set("fs.gs.project.id", args.gcp_project)
@@ -15,8 +12,10 @@ def export_to_gcs(args):
     spark.conf.set("fs.gs.auth.service.account.private.key.id", args.service_account_key_id)
 
     df = spark.sql(args.sql)
-    df.write.format(args.export_format).option("compression", "gzip").option("header", "true").mode("overwrite").save(f"gs://{args.bucket}//{args.prefix}/")
-    
+    if args.export_format == "csv":
+        df.coalesce(1).write.format(args.export_format).option("compression", "gzip").option("header", "true").mode("overwrite").save(f"gs://{args.bucket}//{args.prefix}/")
+    else:
+        df.write.format(args.export_format).option("compression", "gzip").mode("overwrite").save(f"gs://{args.bucket}//{args.prefix}/")
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Spark to GCS unload using SparkPython')
@@ -28,6 +27,6 @@ if __name__ == '__main__':
     parser.add_argument('service_account_email', help='service account which has access to the gcs bucket and path')
     parser.add_argument('service_account_key_id', help='key with which to authorize the gcs')
     parser.add_argument('service_account_key', help='key with which to authorize the gcs')
-    
+
     args = parser.parse_args()
     export_to_gcs(args)
